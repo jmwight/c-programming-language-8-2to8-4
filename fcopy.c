3bbfc38c3336bdbaa9b6ba4bfbf1c3c031064fc7
John Morgan Wight <john.morgan.wight@proton.me>
2025-05-23 17:22:19 -0400

    Add time measuring and printing to fcopy

--- fcopy.c
+++ fcopy.c
@@ -1,10 +1,15 @@
 #include "basicio.h"
+#include "myprintf.h" /* for myprintf, seperate printf function so stdio.h doesn't conflict */
 #include <unistd.h>
+#include <sys/time.h> /* for execution time measurement */
 
 void filecopy(FILE *ifp, FILE *ofp);
 
 int main(int argc, char **argv)
 {
+	/* start time */
+	struct timeval t0, t1, td;
+	gettimeofday(&t0, NULL);
 	initstdio(); /* see if there is a way to have this function run before main */
 	FILE *ifp, *ofp;
        	ifp = fopen(*++argv, "r");
@@ -12,6 +17,9 @@ int main(int argc, char **argv)
 	filecopy(ifp, ofp);
 	fclose(ifp);
 	fclose(ofp);
+	gettimeofday(&t1, NULL);
+	timersub(&t1, &t0, &td);
+	myprintf("\nRuntime for fcopy using explicit bit ops: %ds %dµs\n", td.tv_sec, td.tv_usec);
 }
 
 /* filecopy: copy file ifp to file ofp */
